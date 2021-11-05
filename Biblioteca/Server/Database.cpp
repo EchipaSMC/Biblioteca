d3dd36d6794feda65f56827ab5daadf9d9143b96
IvanRares <ivanrarescosmin@gmail.com>
2021-11-05 18:59:06 +0200

    Added create sql statement function

--- Biblioteca/Server/Database.cpp
+++ Biblioteca/Server/Database.cpp
@@ -5,10 +5,20 @@ Database::Database(const char* name)
 	openDatabase(name);
 }
 
-//statement Database::create_statement(sqlite3* db, const std::string& sql)
-//{
-//	return statement(db,sql);
-//}
+statement Database::create_statement(sqlite3* db, const std::string& sql)
+{
+	if (db)
+	{
+		sqlite3_stmt* stmt = nullptr;
+		int rc = sqlite3_prepare_v2(db, sql.c_str(), sql.length(), &stmt, nullptr);
+		if (rc != SQLITE_OK)
+		{
+			std::cerr << "Unable to create statement: " << sql << " : " << sqlite3_errmsg(db);
+			std::exit(EXIT_FAILURE);
+		}
+		return statement(stmt, sqlite3_finalize);
+	}
+}
 
 void Database::run(sqlite3_stmt* stmt, stmt_callback callback)
 {
