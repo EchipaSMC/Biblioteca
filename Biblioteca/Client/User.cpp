5db6fec0f6cad421f701332dcad5ca93b8a19b2e
YloYuuki <horia.dobrin@gmail.com>
2021-11-07 17:51:18 +0200

    Added getters for username and borrowed books + a display function for borrowed books(unfinished)

--- Biblioteca/Client/User.cpp
+++ Biblioteca/Client/User.cpp
@@ -11,3 +11,28 @@ User::User(const User& user)
 	this->username = user.username;
 	this->borrowedBooks = user.borrowedBooks;
 }
+
+User::User(const User& user)
+{
+	this->username = user.username;
+	this->borrowedBooks = user.borrowedBooks;
+}
+
+std::string User::GetUsername() const
+{
+	return username;
+}
+
+std::vector<Book> User::GetBorrowedBooks() const
+{
+	return borrowedBooks;
+}
+
+void User::ShowBorrowedBooks(const std::string& user, const std::vector<Book>& books)
+{
+	std::cout << "Userul :" << user << "a imprumutat urmatoarele carti: " << std::endl;
+	for (auto& elem : books)
+	{
+		std::cout << elem.getBookID() << " " << elem.getTitle() << std::endl;
+	}
+}
--- Biblioteca/Client/User.h
+++ Biblioteca/Client/User.h
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include "Book.h"
+#include <iostream>
 
 class User {
 private:
@@ -11,4 +12,7 @@ public:
 	User() = default;
 	User(std::string, std::vector<Book>);
 	User(const User&);
+	std::string GetUsername() const;
+	std::vector<Book> GetBorrowedBooks() const;
+	void ShowBorrowedBooks(const std::string&, const std::vector<Book>&);
 };
