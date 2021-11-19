ea080f425c65f401fb3c6e04eeeb1ad675dddde9
IvanRares <ivanrarescosmin@gmail.com>
2021-11-19 17:32:33 +0200

    Added functions for connecting, accepting connection, sending and receiving strings.

--- Biblioteca/TCPSocket/TCPSocket.cpp
+++ Biblioteca/TCPSocket/TCPSocket.cpp
@@ -37,7 +37,82 @@ TCPSocket::~TCPSocket()
 	}
 }
 
+bool TCPSocket::Connect()
+{
+	std::cout << "Connecting..." << std::endl;
+	int iResult = connect(sock, (SOCKADDR*)&addrServer, sizeof(addrServer));
+	if (iResult == SOCKET_ERROR) {
+		closesocket(sock);
+		std::cerr << "Unable to connect to server: " << WSAGetLastError() << std::endl;
+		return false;
+	}
 
+	return true;
+}
+
+bool TCPSocket::Listen()
+{
+	if (bind(sock, (SOCKADDR*)&addrServer, sizeof(addrServer)) == SOCKET_ERROR) {
+		std::cerr << "Bind failed with error: " << WSAGetLastError() << std::endl;
+		return false;
+	}
+
+	if (listen(sock, SOMAXCONN) == SOCKET_ERROR) {
+		std::cerr << "Listen failed with error: " << WSAGetLastError() << std::endl;
+		return false;
+	}
+	return true;
+}
+
+TCPSocket TCPSocket::Accept()
+{
+	SOCKET clientSocket = accept(sock, NULL, NULL);
+	if (clientSocket == INVALID_SOCKET) {
+		throw std::string("Accept failed with error: ") + std::to_string(WSAGetLastError());
+	}
+	return TCPSocket(clientSocket);
+}
+
+bool TCPSocket::Send(const std::string& data, int size)
+{
+	int iSendResult = send(sock, (char*)&size, sizeof(int), 0);
+	if (iSendResult == SOCKET_ERROR) {
+		std::cerr << "Send failed with error: " << WSAGetLastError() << std::endl;
+		return false;
+	}
+
+	iSendResult = send(sock, data.c_str(), data.size(), 0);
+	if (iSendResult == SOCKET_ERROR) {
+		std::cerr << "Send failed with error: " << WSAGetLastError() << std::endl;
+		return false;
+	}
+	return true;
+}
+
+bool TCPSocket::Receive(std::string& data)
+{
+	int size;
+	int iReceiveResult = recv(sock, (char*)&size, sizeof(int), 0);
+	if (iReceiveResult == 0)
+		std::cout << "Connection closed\n" << std::endl;
+	else if (iReceiveResult < 0)
+	{
+		std::cerr << "recv failed with error: " << WSAGetLastError();
+		return false;
+	}
+	char* recvbuf = new char[size];
+
+	iReceiveResult = recv(sock, recvbuf, size, 0);
+	if (iReceiveResult > 0) {
+		for (int i = 0; i < iReceiveResult; i++) data += recvbuf[i];
+	}
+	if (iReceiveResult < 0)
+	{
+		std::cerr << "receive failed: " << WSAGetLastError() << std::endl;
+		return false;
+	}
+	return true;
+}
 
 struct WinsockInitializer
 {
